e95b3761f88a91efb98e9a0f2330bf6ac13a5256
wellington lima <well.oliveira1313@gmail.com>
2025-10-09 22:24:16 -0300

    Adicionando navbar,main,footer e etc.

---
+++ src/script.js
@@ -0,0 +1,26 @@
+//Máscara de valor em R$
+const inputValor = document.getElementById('value');
+inputValor.addEventListener('input', (e) => {
+    let valor = e.target.value.replace(/\D/g, '');
+    valor = (valor / 100).toFixed(2) + '';
+    valor = valor.replace('.', ',');
+    valor = valor.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    e.target.value = 'R$ ' + valor;
+});
+
+// Script de Validação
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
